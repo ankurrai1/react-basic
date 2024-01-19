0139cc2c27777b8ccd93a00f8e5c91ecfb475b66
ankurrai <ankur.rai@alefeducation.com>
2024-01-20 02:12:24 +0400

    split into app.js and creadted basic structur using react function

---
+++ App.js
@@ -0,0 +1,27 @@
+/**
+ * Create following pattern
+ * <div id="parent" >
+ *    <div id="child">
+ *       <h1>Hello I am h1 tag </h1>
+ *       <h2>Hello I am h2 tag </h2>
+ *    </div>
+ * </div>
+ */
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement(h1, {}, "I am H1 tag"),
+    React.createElement("h1", {}, "I am H2 tag"),
+  ])
+);
+
+const heading = React.createElement(
+  "h1",
+  { id: "head" },
+  "hello world from react !"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+// root.render(parent)
